d07e68a351a8f80179e59c591a48780ff6c5c96b
maja42 <2649265+maja42@users.noreply.github.com>
2025-01-12 16:47:13 +0100

    Improved hex-inversion: black/white for better contrast

--- app/js/algo.js
+++ app/js/algo.js
@@ -12,13 +12,24 @@ function rgbToHex(r, g, b) {
 }
 
 function inverseHex(hex) {
-    return (
-        "#" +
-        hex
-            .match(/[a-f0-9]{2}/gi)
-            .map((e) => ((255 - parseInt(e, 16)) | 0).toString(16).replace(/^([a-f0-9])$/, "0$1"))
-            .join("")
-    );
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    var r = parseInt(hex.slice(0, 2), 16),
+        g = parseInt(hex.slice(2, 4), 16),
+        b = parseInt(hex.slice(4, 6), 16);
+
+    // https://stackoverflow.com/a/3943023/112731
+    return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+        ? '#000000'
+        : '#FFFFFF';
 }
 
 function uuidv4() {
